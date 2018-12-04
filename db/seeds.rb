149bb22da85c2399d555430fc5351de0b0030426
github username <github email address>
2018-12-04 18:12:58 -0500

    added um favorites and um relationships hahaha

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,28 @@
-
+User.destroy_all
+Quote.destroy_all
 
 User.create(username: "kikidunzz")
 User.create(username: "aariasgonz")
-User.create(username: "bigboiii3")
+bigboi = User.create(username: "bigboiii3")
 User.create(username: "avocadotoast2")
 User.create(username: "yerrrrrr")
+
+Quote.create(quote:"I like to look cool and dress nice. Lil Uzi is a rock star. Just know that.", author:"Lil Uzi Vert", category:"funny")
+Quote.create(quote: "There's enough money for everybody. See, I can't do what Yachty does. But then again, Yachty can't do what I do.", author: "Lil Uzi Vert", category:"motivational")
+Quote.create(quote: "I never wanted to be no rapper", author:"Lil Uzi Vert", category:"depressing")
+
+Quote.create(quote: "I am good, but not an angel. I do sin, but I am not the devil. I am just a small girl in a big world trying to find someone to love", author:"Marilyn Monroe", category:"love")
+Quote.create(quote: "Fear is stupid. So are regrets", author:"Marilyn Monroe", category:"motivational")
+Quote.create(quote: "Experts on romance say for a happy marriage there has to be more than a passionate love. For a lasting union, they insist, there must be a genuine liking for each other. Which, in my book, is a good definition for friendship", author:"Marilyn Monroe", category:"friendship")
+
+Quote.create(quote: "I'm a good man, and I'm gonna become a better man", author:"Chance the Rapper", category:"motivational")
+Quote.create(quote: "I didn't know love until I had my daughter. I didn't know its bounds", author:"Chance the Rapper", category:"love")
+Quote.create(quote: "Walking with a friend in the dark is better than walking alone in the light", author:"Helen Keller", category:"friendship")
+Quote.create(quote: "Do I not destroy my enemies when I make them my friends?", author:"Abraham Lincoln", category:"friendship")
+Quote.create(quote: "I don't fear death so much as I fear its prologues: loneliness, decrepitude, pain, debilitation, depression, senility. After a few years of those, I imagine death presents like a holiday at the beach", author:"Mary Roach", category:"depression")
+
+Favorite.create(user_id: 3, quote_id: 3)
+Favorite.create(user_id: 3, quote_id: 2)
+Favorite.create(user_id: 3, quote_id: 7)
+Favorite.create(user_id: 3, quote_id: 8)
+Favorite.create(user_id: 3, quote_id: 5)
--- lib/user.rb
+++ lib/user.rb
@@ -1,5 +1,13 @@
-#require_relative "./environment.rb"
+require "pry"
 
 class User < ActiveRecord::Base
-
+  has_many :favorites
+  has_many :quotes, through: :favorites
+  # def favorites
+  #   Favorite.all.select {|fave| fave.user_id == self.id }
+  # end
+  #
+  # def quotes
+  #   self.favorites.map {|favorite| Quote.find(favorite.quote_id) }
+  # end
 end
