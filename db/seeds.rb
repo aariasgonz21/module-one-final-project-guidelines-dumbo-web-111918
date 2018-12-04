39be885aacc84c1d1b6fa4ea220fc5a6e0fa32dd
github username <github email address>
2018-12-04 14:52:04 -0500

    created users table + dummy users

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+
+
+User.create(username: "kikidunzz")
+User.create(username: "aariasgonz")
+User.create(username: "bigboiii3")
+User.create(username: "avocadotoast2")
+User.create(username: "yerrrrrr")
---
+++ lib/user.rb
@@ -0,0 +1,5 @@
+#require_relative "./environment.rb"
+
+class User < ActiveRecord::Base
+
+end
